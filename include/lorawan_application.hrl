ef3810a77d0532def67dcb910b7de7b60bd92c9b
Petr Gotthard <petr.gotthard@centrum.cz>
2017-02-28 22:54:15 +0100

    A bit of documentation

--- include/lorawan_application.hrl
+++ include/lorawan_application.hrl
@@ -4,17 +4,95 @@
 % Distributed under the terms of the MIT License. See the LICENSE file.
 %
 
--record(user, {name, pass}).
--record(gateway, {mac, tx_rfch, netid, gpspos, gpsalt}).
--record(device, {deveui, region, app, appid, appeui, appkey, link, can_join, last_join,
-    fcnt_check, adr_flag_set, adr_set}).
--record(link, {devaddr, region, app, appid, nwkskey, appskey, fcntup, fcntdown, last_rx,
-    fcnt_check, adr_flag_use, adr_flag_set, adr_use, adr_set, devstat_time, devstat_fcnt, devstat}).
--record(pending, {devaddr, confirmed, phypayload}).
--record(txframe, {frid, datetime, devaddr, txdata}).
--record(rxframe, {frid, mac, rssi, lsnr, freq, datr, codr, devaddr, fcnt, port, data, region, datetime, devstat}).
+-type eui() :: <<_:128>>.
+-type seckey() :: <<_:256>>.
+-type devaddr() :: <<_:64>>.
+-type frid() :: <<_:64>>.
+-type intervals() :: [{integer(), integer()}].
+-type adr_config() :: {integer(), integer(), intervals()}.
+-type devstat() :: {integer(), integer()}.
 
--record(rxdata, {port, data, last_lost=false, shall_reply=false}).
--record(txdata, {confirmed=false, port, data, pending=false}).
+-record(user, {
+    name :: nonempty_string(),
+    pass :: string()}).
+
+-record(gateway, {
+    mac :: binary(),
+    tx_rfch :: integer(), % rf chain for downlinks
+    netid :: binary(), % network id
+    gpspos :: {number(), number()}, % {latitude, longitude}
+    gpsalt :: number()}). % altitude
+
+-record(device, {
+    deveui :: eui(),
+    region :: binary(),
+    app :: binary(),
+    appid :: any(), % application arguments
+    appeui :: eui(),
+    appkey :: seckey(),
+    link :: devaddr(),
+    can_join :: boolean(),
+    last_join :: calendar:datetime(),
+    fcnt_check :: integer(),
+    adr_flag_set :: boolean(), % server requests
+    adr_set :: adr_config()}). % requested after join
+
+-record(link, {
+    devaddr :: devaddr(),
+    region :: binary(),
+    app :: binary(),
+    appid :: any(), % application arguments
+    nwkskey :: seckey(),
+    appskey :: seckey(),
+    fcntup :: integer(), % last uplink fcnt
+    fcntdown :: integer(), % last downlink fcnt
+    last_rx :: calendar:datetime(),
+    fcnt_check :: integer(),
+    adr_flag_use :: boolean(), % device supports
+    adr_flag_set :: boolean(), % server requests
+    adr_use :: adr_config(), % used
+    adr_set :: adr_config(), % requested
+    devstat_time :: calendar:datetime(),
+    devstat_fcnt :: integer(),
+    devstat :: devstat()}). % {battery, margin}
+
+-record(rxdata, {
+    port :: integer(),
+    data :: binary(),
+    last_lost=false :: boolean(),
+    shall_reply=false :: boolean()}).
+
+-record(txdata, {
+    confirmed=false :: boolean(),
+    port :: integer(),
+    data :: binary(),
+    pending=false :: boolean()}).
+
+-record(pending, {
+    devaddr :: devaddr(),
+    confirmed :: boolean(),
+    phypayload :: binary()}).
+
+-record(txframe, {
+    frid :: frid(), % unique identifier
+    datetime :: calendar:datetime(),
+    devaddr :: devaddr(),
+    txdata :: #txdata{}}).
+
+-record(rxframe, {
+    frid :: frid(), % unique identifier
+    mac :: binary(), % gateway used
+    rssi :: number(),
+    lsnr :: number(),
+    freq :: number(),
+    datr :: binary(),
+    codr :: binary(),
+    devaddr :: devaddr(),
+    fcnt :: integer(),
+    port :: integer(),
+    data :: binary(),
+    region :: binary(),
+    datetime :: calendar:datetime(),
+    devstat :: devstat()}). % {battery, margin}
 
 % end of file
--- src/lorawan_application.erl
+++ src/lorawan_application.erl
@@ -9,9 +9,9 @@
 
 -callback init(App :: binary()) ->
     ok | {ok, [Path :: cowboy_router:route_path()]}.
--callback handle_join(DevAddr :: binary(), App :: binary(), AppID :: binary()) ->
+-callback handle_join(DevAddr :: binary(), App :: binary(), AppArgs :: binary()) ->
     ok | {error, Error :: term()}.
--callback handle_rx(DevAddr :: binary(), App :: binary(), AppID :: binary(), RxData :: #rxdata{}) ->
+-callback handle_rx(DevAddr :: binary(), App :: binary(), AppArgs :: binary(), RxData :: #rxdata{}) ->
     ok | retransmit |
     {send, Port :: integer(), Data :: #txdata{}} |
     {error, Error :: term()}.
